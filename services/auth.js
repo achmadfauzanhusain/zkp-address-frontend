843853ad92f80f40e38cf72823e35d4b77b2a7eb
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-02 20:39:44 +0700

    calling login API

---
+++ services/auth.js
@@ -0,0 +1,12 @@
+import callApi from "@/lib/api";
+
+const ROOT_API = "http://localhost:8000"
+
+export async function setLogin(data) {
+    const url = `${ROOT_API}/auth/login`
+    return callApi({
+        url,
+        method: "POST",
+        data,
+    })
+}
