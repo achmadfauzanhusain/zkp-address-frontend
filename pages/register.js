f5539031bae730a2d1c0cf9bc0aa4ea0d98b6537
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-04 23:00:16 +0700

    install dependencies for appkit

---
+++ pages/register.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+const Register = () => {
+    return (
+        <div className="mt-24 px-32">
+            <div className="mt-24">
+                <h1 className="text-3xl font-semibold">Register</h1>
+                <p className="text-sm opacity-75">you can create ur account!</p>
+
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register;
