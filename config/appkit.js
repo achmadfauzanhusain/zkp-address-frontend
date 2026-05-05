183f399940d255fe4881b76580a9f67295538160
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-05-05 21:00:09 +0700

    setup config for appkit

---
+++ config/appkit.js
@@ -0,0 +1,15 @@
+import { EthersAdapter } from '@reown/appkit-adapter-ethers'
+import { mainnet } from '@reown/appkit/networks'
+
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+if (!projectId) throw new Error('Project ID tidak ditemukan')
+
+export const network = [mainnet]
+export const ethersAdapter = new EthersAdapter()
+
+export const metadata = {
+  name: 'My App',
+  description: 'My Web3 App',
+  url: 'https://myapp.com',
+  icons: ['https://myapp.com/icon.png']
+}
